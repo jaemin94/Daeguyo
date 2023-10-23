0f8ab97ddcdfe8a3793b4556f2f0fcd4933dfa2d
baekboowoo <hds980302@gmail.com>
2023-10-23 09:51:24 +0900

    loginbuttonchange

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -30,7 +30,7 @@ public class DataSourceConfig {
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/daeguyo");
 		dataSource.setUsername("root");
-		dataSource.setPassword("1234");
+		dataSource.setPassword("123456789");
 
 		return dataSource;
 	}
