dd990d2e3e190ee3ea021a71f997e0cb649ed1c8
kshkshksh <ksh931101@naver.com>
2023-10-25 10:14:12 +0900

    1025ksh

--- src/main/resources/static/js/selectRest.js
+++ src/main/resources/static/js/selectRest.js
@@ -38,3 +38,90 @@ document.querySelector('.order_list').addEventListener('click', function(e) {
       }
     }
   });
+document.addEventListener("DOMContentLoaded", function () {
+  // select 요소를 가져옵니다.
+  var select = document.querySelector(".order_list");
+
+  // rest_list 요소를 가져옵니다.
+  var restList = document.querySelector(".rest_list");
+
+  // 초기 정렬 순서를 저장할 변수입니다.
+  var defaultOrder = Array.from(restList.querySelectorAll(".Rest"));
+
+  // select 요소가 변경될 때 실행될 함수를 정의합니다.
+  select.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var target = event.target;
+    if (target.tagName === "A") {
+      var sortType = target.getAttribute("data-sort");
+      sortRestaurants(sortType);
+    }
+  });
+
+  // 주문 리스트 정렬 함수
+  function sortRestaurants(sortType) {
+    if (sortType === "default") {
+      // 기본순으로 정렬
+      restList.innerHTML = "";
+      for (var i = 0; i < defaultOrder.length; i++) {
+        restList.appendChild(defaultOrder[i]);
+      }
+    } else if (sortType === "rating") {
+      // 별점순으로 정렬
+      var restaurants = Array.from(defaultOrder);
+
+      restaurants.sort(function (a, b) {
+        var starA = parseFloat(a.querySelector(".star-score").textContent.replace(/[^0-9.]/g, ""));
+        var starB = parseFloat(b.querySelector(".star-score").textContent.replace(/[^0-9.]/g, ""));
+        return starB - starA;
+      });
+
+      updateRestaurantList(restaurants);
+    } else if (sortType === "delivery") {
+      // 배달팁 낮은 순으로 정렬
+      var restaurants = Array.from(defaultOrder);
+
+      restaurants.sort(function (a, b) {
+        var deliveryA = parseInt(a.querySelector(".fee-list").textContent.replace(/[^0-9]/g, ""));
+        var deliveryB = parseInt(b.querySelector(".fee-list").textContent.replace(/[^0-9]/g, ""));
+        return deliveryA - deliveryB;
+      });
+
+     updateRestaurantList(restaurants);
+    } else if (sortType === "price") {
+       // 주문금액 낮은 순으로 정렬
+       var restaurants = Array.from(defaultOrder);
+
+       restaurants.sort(function (a, b) {
+         var priceA = parseInt(a.querySelector(".pay-list").textContent.replace(/[^0-9]/g, ""));
+         var priceB= parseInt(b.querySelector(".pay-list").textContent.replace(/[^0-9]/g, ""));
+         return priceA - priceB;
+       });
+
+       updateRestaurantList(restaurants);
+    } else if (sortType === "etc") {
+
+           /* restaurant.res_name의 이름을 랜덤하게 배열 */
+
+           var restaurants=Array.from(defaultOrder);
+
+          restaurants.sort(function(){
+             return Math.random() - 0.5;
+          });
+
+          updateRestaurantList(restaurants);
+
+
+       }
+     }
+
+       function updateRestaurantList(restaurants){
+         restList.innerHTML="";
+         for(var i=0;i<restaurants.length;i++){
+              restList.appendChild(restaurants[i]);
+         }
+       }
+
+    });
+
